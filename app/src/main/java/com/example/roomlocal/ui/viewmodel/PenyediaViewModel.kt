d9b3f4b3120d3e5496129b5f52846914ffcce0f7
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-17 03:05:47 +0700

    Melengkapi PenyediaViewModel.kt dengan initializer

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.roomlocal.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,6 +14,25 @@ object PenyediaViewModel {
         initializer {
             MahasiswaViewModel(
                 krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer{
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer{
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+
+        initializer{
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
             )
         }
     }
