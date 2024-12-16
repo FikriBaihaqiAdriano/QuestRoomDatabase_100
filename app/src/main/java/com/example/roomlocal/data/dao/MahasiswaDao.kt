3728f5036a4e5455a6378693102c62949b5268ed
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-16 22:14:00 +0700

    mengambil semua data mahasiswa yang tersimpan di database diurutkan berdasarkan nama secara ascending (ASC). hasilnya berupa aliran data (Flow) yang memungkinkan aplikasi untuk merespons perubahan data secara real-time

--- app/src/main/java/com/example/roomlocal/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/roomlocal/data/dao/MahasiswaDao.kt
@@ -2,7 +2,9 @@ package com.example.roomlocal.data.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.roomlocal.data.entitiy.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
@@ -10,4 +12,8 @@ interface MahasiswaDao {
     suspend fun insertMahasiswa(
         mahasiswa: Mahasiswa
     )
+
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa () : Flow<List<Mahasiswa>>
+
 }
