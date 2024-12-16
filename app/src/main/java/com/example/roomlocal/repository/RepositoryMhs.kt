b4b56caaf6892d9705107fdaafca34c25c0b62ad
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-16 22:24:55 +0700

    memanggil fungsi getAllMahasiswa dari MahasiswaDao untuk mendapatkan semua data mahasiswa dalam bentuk aliran data(Flow)

--- app/src/main/java/com/example/roomlocal/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocal/repository/RepositoryMhs.kt
@@ -1,7 +1,9 @@
 package com.example.roomlocal.repository
 
 import com.example.roomlocal.data.entitiy.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 }
