d1296a8f02d4e5dfd7e2a3e98046f9925eefe841
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-17 00:08:45 +0700

    memanfaatkan fungsi updateMahasiswa dari MahasiswaDao untuk memperbarui data mahasiswa

--- app/src/main/java/com/example/roomlocal/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocal/repository/RepositoryMhs.kt
@@ -8,4 +8,5 @@ interface RepositoryMhs {
     fun getAllMahasiswa(): Flow<List<Mahasiswa>>
     fun getMhs(nim: String): Flow<Mahasiswa>
     suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
