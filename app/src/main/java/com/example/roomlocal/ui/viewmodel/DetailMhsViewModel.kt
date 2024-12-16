01d8bbbb67ccde8c61d1019dbb9f0624ece8d5a9
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-17 01:15:38 +0700

    Mengambil data mahasiswa berdasarkan NIM dari repository dan memprosesnya menjadi DetailUiState.

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
@@ -3,19 +3,29 @@ package com.example.roomlocal.ui.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.roomlocal.repository.RepositoryMhs
-
+import com.example.roomlocal.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 
     ) : ViewModel() {
+    private val nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
-
-
-
-
-
-
-}
-
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
