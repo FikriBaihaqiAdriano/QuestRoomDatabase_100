9ae4078631b64139810ca97bf711c29eadc7d290
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-17 01:07:58 +0700

    ViewModel untuk halaman Detail Mahasiswa. Menggunakan SavedStateHandle untuk mengambil argumen navigasi (NIM) dan RepositoryMhs untuk mengakses data mahasiswa.

---
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.roomlocal.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.roomlocal.repository.RepositoryMhs
+
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+
+    ) : ViewModel() {
+
+
+
+
+
+
+
+}
+
