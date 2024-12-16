a231b7fdd4bc63d6f2818772520c5681c8147408
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-12-17 02:55:18 +0700

    variabel untuk menyimpan nim mahasiswa yang akan diperbarui

--- app/src/main/java/com/example/roomlocal/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocal/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,14 +1,22 @@
 package com.example.roomlocal.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.roomlocal.repository.RepositoryMhs
+import com.example.roomlocal.ui.navigation.DestinasiUpdate
 
 class UpdateMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
-) : ViewModel() {}
+) : ViewModel() {
 
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
 
 
 
